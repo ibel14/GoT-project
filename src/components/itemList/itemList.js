2fbd9b88eb8eb5cd42384311f4c79466b050b049
ponaehali <iambelousov@gmail.com>
2021-01-26 21:03:47 +0200

    #104 refactoring with hooks

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,69 +1,46 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import './itemList.css';
 import Spinner from '../spinner';
-import gotService from '../../services/gotService';
 
-class ItemList extends Component {
+function ItemList({getData, onItemSelected, renderItem}){
 
-    renderItems(arr) {
-        return arr.map((item, i) => {
+    const [itemList, updateList] = useState([]);
 
+    useEffect(() => {
+        getData()
+        .then( (data) => {
+            updateList(data)
+            })
+        }, [])
+
+    function renderItems(arr) {
+        return arr.map((item) => {
             const {id} = item;
-            const label = this.props.renderItem(item);
+
+            const label = renderItem(item);
 
             return (
                 <li 
                 key={id}
                 className="list-group-item"
-                onClick={ () => this.props.onItemSelected(id)}>
+                onClick={ () => onItemSelected(id)}>
                     {label}
                 </li>
             )
         })
     }
 
-    render() {
-        const {data} = this.props;
-        const items = this.renderItems(data);
+        if (!itemList) {
+                return <Spinner/>
+            }
+            
+            const items = renderItems(itemList);
 
-        return (
-            <ul className="item-list list-group">
-                {items}
-            </ul>
-        );
-    }
+            return (
+                <ul className="item-list list-group">
+                    {items}
+                </ul>
+            );
 }
 
-ItemList.defaultProps = {
-    onItemSelected: () => {}
-}
-
-const withData = (View, getData) => {
-    
-    return class extends Component {
-        state = {
-            data: null
-        }
-
-        componentDidMount() {
-                
-            getData()
-            .then( (data) => {
-                this.setState({
-                    data
-                })
-            })
-        }
-
-        render() {
-            const {data} = this.state;
-
-            if (!data) {
-                    return <Spinner/>
-                }
-                return <View {...this.props} data={data}/>
-        }
-    }
-}
-const {getAllCharacters} = new gotService();
-export default withData(ItemList, getAllCharacters);
+export default ItemList();
